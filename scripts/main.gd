04108e69ff708b58c16dcc4b0c507190a33392dd
Mel <garrillofeeling@gmail.com>
2024-09-11 09:18:48 -0400

    Set up main scene

--- scripts/main.gd
+++ scripts/main.gd
@@ -4,7 +4,7 @@ const FileHandler = preload("res://scripts/file_handle/file_handler.gd")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var fh = FileHandler.new()
-	fh.load_from_file("user://assets/dialogues/test.txt")
+	print(fh.load_from_file("res://assets//dialogues//test.txt"))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
