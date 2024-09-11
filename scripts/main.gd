5b63354ef4d9dbd618bc58d995db0126ca23dc8e
Mel <garrillofeeling@gmail.com>
2024-09-11 09:08:42 -0400

    Create main scene

---
+++ scripts/main.gd
@@ -0,0 +1,12 @@
+extends Node2D
+const FileHandler = preload("res://scripts/file_handle/file_handler.gd")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var fh = FileHandler.new()
+	fh.load_from_file("user://assets/dialogues/test.txt")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
