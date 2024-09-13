f1cbe067330ee8e620fc455b170f125a05fcbc6b
Mel <garrillofeeling@gmail.com>
2024-09-13 11:21:10 -0400

    Change emotion setting to character

--- scripts/characters/character.gd
+++ scripts/characters/character.gd
@@ -1,9 +1,15 @@
 extends Node2D
 class_name Character
 @export var id = ""
+@onready var state_manager: StateManager = $StateManager
+
 var mood = ""
 var is_talking = false
 
+func change_emotion(emotion):
+	state_manager.change_state(emotion)
+	set_mood(emotion)
+	
 func set_mood(new_mood):
 	if new_mood == mood:
 		return
