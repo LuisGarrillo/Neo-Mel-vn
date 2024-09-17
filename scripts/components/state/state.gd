4d41acf73917071036f6e7201cb341b4d41d6857
Mel <garrillofeeling@gmail.com>
2024-09-17 09:54:11 -0400

    Change state functionality

--- scripts/components/state/state.gd
+++ scripts/components/state/state.gd
@@ -9,11 +9,12 @@ signal change
 
 var counter: int = 0
 var root_position : Vector2
+var root: Node2D
 
-func start():
+func start(args):
 	started.emit()
 
-func update():
+func update(_delta):
 	pass
 	
 func finish():
