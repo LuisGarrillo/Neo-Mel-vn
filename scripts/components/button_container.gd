7d5979b6a895078a28039ad4bca39f95275d0e6c
Mel <garrillofeeling@gmail.com>
2024-09-19 11:20:00 -0400

    Fix button container

--- scripts/components/button_container.gd
+++ scripts/components/button_container.gd
@@ -9,12 +9,18 @@ var frame_counter: int = 0
 func _ready():
 	buttons = get_children()
 	selected_button = buttons[0]
-	frame_counter += 1
+	selected_button.grab_focus()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	check_input()
 	frame_counter += 1
+
+func manage_focus():
+	frame_counter = 0
+	selected_button.release_focus()
+	selected_button = buttons[index]
+	selected_button.grab_focus()
 
 func check_input():
 	if Input.is_action_just_pressed("accept") and selected_button:
@@ -22,13 +28,7 @@ func check_input():
 		return
 	elif Input.is_action_pressed("down") and frame_counter > 10:
 		index = (index + 1) % len(buttons)
-		frame_counter = 0
+		manage_focus()
 	elif Input.is_action_pressed("up") and frame_counter > 10:
 		index = (index - 1) % len(buttons)
-		frame_counter = 0
-	
-	if selected_button:
-		selected_button.release_focus()
-	
-	selected_button = buttons[index]
-	selected_button.grab_focus()
+		manage_focus()
