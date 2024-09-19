970136fd10120b2cb363d0f0377ee7885526c069
Mel <garrillofeeling@gmail.com>
2024-09-19 11:20:47 -0400

    Create pause menu

---
+++ scripts/ui/pause.gd
@@ -0,0 +1,23 @@
+extends Control
+class_name PauseMenu
+signal load
+signal status
+signal resume
+signal title
+
+
+func _on_load_btn_pressed() -> void:
+	load.emit()
+
+
+func _on_status_btn_pressed() -> void:
+	status.emit()
+
+
+func _on_resume_btn_pressed() -> void:
+	print("resume")
+	resume.emit()
+
+
+func _on_back_title_btn_pressed() -> void:
+	title.emit()
