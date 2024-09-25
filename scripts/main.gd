ad9109f5fd9f5ccc7adce7be5d42de708aa1b78b
Mel <garrillofeeling@gmail.com>
2024-09-25 10:36:06 -0400

    Add back from save load menu

--- scripts/main.gd
+++ scripts/main.gd
@@ -7,6 +7,7 @@ var on_title : bool = true
 var game
 var save_load
 var pause_menu : PauseMenu
+var go_back_screens: Dictionary = {}
 
 func _process(_delta: float) -> void:
 	check_input()
@@ -15,7 +16,7 @@ func connect_pause_signals():
 	pause_menu.load.connect(load_game)
 	pause_menu.status.connect(check_status)
 	pause_menu.resume.connect(handle_pause)
-	pause_menu.title.connect(back_to_title)
+	pause_menu.title.connect(go_back)
 
 func check_input():
 	if Input.is_action_just_pressed("pause") and !on_title:
@@ -48,6 +49,9 @@ func load_game() -> void:
 		remove_child(title)
 	save_load = SAVE_LOAD.instantiate()
 	save_load.set_up_mode("load")
+	save_load.previous_screen = "title"
+	save_load.return_back.connect(go_back)
+	go_back_screens["save_load"] = save_load
 	add_child(save_load)
 
 func game_exit() -> void:
@@ -55,10 +59,20 @@ func game_exit() -> void:
 
 func check_status():
 	pass
+
+func go_back(from, to):
+	if from == "save_load":
+		remove_child(save_load)
+		toggle_pause()
+		save_load = null
+	elif from == "pause":
+		handle_pause()
 	
+	if to == "title":
+		back_to_title()
+
 func back_to_title():
 	on_title = true
-	handle_pause()
 	var children = get_children()
 	for child in children:
 		remove_child(child)
