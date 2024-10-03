51276ced2d48446e5e5ea2238d9eef9b2b1586b2
Mel <garrillofeeling@gmail.com>
2024-10-03 10:25:37 -0400

    Add coffee pouring toggle when tilting pot

--- scripts/objects/pot.gd
+++ scripts/objects/pot.gd
@@ -1,7 +1,11 @@
 extends Node2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var coffee_pour: Node2D = $CoffeePour
 
 var finished_rotation = false
+
+func _ready() -> void:
+	coffee_pour.position = $Marker2D.position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -11,9 +15,12 @@ func _process(_delta: float) -> void:
 func check_input():
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and not finished_rotation:
 		animation_player.play("pour")
+		
 	elif not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and finished_rotation:
 		animation_player.play("return")
 
-
 func set_finished_rotation():
 	finished_rotation = !finished_rotation
+	
+func toggle_pouring():
+	coffee_pour.toggle()
