9945fa46ad30eedfbd85c82314b2b621a0693192
Mel <garrillofeeling@gmail.com>
2024-10-23 12:16:35 -0400

    Add temperature switching handling

--- scripts/managers/pouring_manager.gd
+++ scripts/managers/pouring_manager.gd
@@ -2,6 +2,8 @@ extends Node
 @onready var timer: Timer = $Timer
 @onready var filter: Node2D = $Filter
 
+signal change_temperature
+
 var pattern
 
 # Called when the node enters the scene tree for the first time.
@@ -10,6 +12,16 @@ func _ready() -> void:
 
 func set_up(pouring_pattern):
 	pattern = pouring_pattern
+
+func update(state):
+	if state == "wait":
+		filter.wait()
+	else:
+		filter.start(state)
+
+func change_temperature_intermediary(hot: bool):
+	change_temperature.emit(hot)
+		
 
 func _on_timer_timeout() -> void:
 	print(filter.pour_accumulator)
