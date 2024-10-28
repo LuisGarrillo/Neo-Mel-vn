c92a9a1cfcbfdf1adce7eed6d30b364894548859
Mel <garrillofeeling@gmail.com>
2024-10-28 12:24:58 -0400

    Fix add on button
    Add signal connection and fix sprite behaviour

--- scripts/ui/add_on_button.gd
+++ scripts/ui/add_on_button.gd
@@ -1,10 +1,13 @@
 extends Control
+class_name AddOnButton
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var button: Button = $Button
-@onready var icon_sprite: Sprite2D = $IconSprite
+@onready var icon_sprite: Sprite2D = $Button/IconSprite
 
 @export var ingredient_name: String
 @export var icon: Texture2D
+
+signal pressed
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,3 +19,7 @@ func select() -> void:
 	
 func deselect() -> void:
 	animation_player.play("Deselect")
+	
+func button_pressed() -> void:
+	pressed.emit(self)
+	
